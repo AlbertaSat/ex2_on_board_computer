4ad8984e22ec130179c8d67e831c4055523f8ce2
joshdellaz <josh.lazaruk@gmail.com>
2019-03-26 20:56:20 -0600

    added Ceedling w/ compression module & pdf tutorial

---
+++ EX-2/src/compression.h
@@ -0,0 +1,5 @@
+#ifndef _COMPRESSION_H
+#define _COMPRESSION_H
+
+
+#endif // _COMPRESSION_H
---
+++ EX-2/test/test_compression.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "compression.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_compression_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement compression");
+}
