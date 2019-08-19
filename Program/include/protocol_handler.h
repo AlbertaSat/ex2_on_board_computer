ce7be37ec0b2ef58a6207af7db8342f798f99f00
EvanGee <evangiese77@gmail.com>
2019-08-19 15:09:18 -0600

    Move that there

--- Program/include/protocol_handler.h
+++ Program/include/protocol_handler.h
@@ -20,17 +20,6 @@ void set_data_length(char*packet, uint16_t data_len);
 uint32_t build_nak_packet(char *packet, uint32_t start, Request *req);
 uint16_t get_data_length(char*packet);
 
-Request *put_request(
-            char *source_file_name,
-            char *destination_file_name,
-            uint8_t segmentation_control,
-            uint8_t fault_handler_overides,
-            uint8_t flow_lable,
-            uint8_t transmission_mode,
-            char* messages_to_user,
-            char* filestore_requests,
-            Client *client,
-            Protocol_state *p_state
-            );
+
 
 #endif
