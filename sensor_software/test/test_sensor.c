10b75fba3676e76bb23676e80fd05b184efe1a32
Vasu Gupta <punarvas@ualberta.ca>
2020-06-03 23:42:00 -0600

    adding ceedling framework

---
+++ sensor_software/test/test_sensor.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+
+#include "sensor.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_sensor_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement sensor");
+}
