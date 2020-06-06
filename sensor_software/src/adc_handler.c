1d843d1159595094f587d9ae54ac62765061404f
Vasu Gupta <punarvas@ualberta.ca>
2020-06-06 04:23:03 -0600

    added some adc_handler stuff/comments

--- sensor_software/src/adc_handler.c
+++ sensor_software/src/adc_handler.c
@@ -1,11 +1,27 @@
 #include "adc_handler.h"
 
 
-void adc_init(ADC_Handler *handl) {
+static spiDAT1_t sConfDat;
+
+
+void adc_init(ADC_Handler *handl, spiBASE_t *spi) {
     //Initialize ADC defaults and SPI here
+    handl->adc_res          = 4096;
+    handl->control_reg_val  = 0;
+    handl->spi_base_addr    = spi;
+    //STORE SPI CS PIN ADDR
+
+    //Initialize spi stuff
+    spiInit();
+    
+    //These change for each ADC (different SPI CS line) MIGHT NEED TO MOVE/CHANGE THIS
+    sConfDat.CS_HOLD = TRUE;
+    sConfDat.WDEL    = TRUE;
+    sConfDat.DFSEL   = SPI_FMT_0;
+    sConfDat.CSNR    = 0xFF;
 }
 
-void adc_set_control_reg(ADC_Handler *handl, unsigned short value) {
+void adc_set_control_reg(ADC_Handler *handl, uint16_t value) {
     /*
      * create 8-bit buffer
      * split value into two 8-bit parts
@@ -13,20 +29,26 @@ void adc_set_control_reg(ADC_Handler *handl, unsigned short value) {
      * 
      * 
      */
-    unsigned char buffer[2] = {0,0};
+    // uint16_t buffer[2] = {0,0};
 
-    buffer[0] = (value & 0xFF00) >> 8;
-    buffer[1] = (value & 0x00FF);
+    // buffer[0] = (value & 0xFF00) >> 8;
+    // buffer[1] = (value & 0x00FF);
     
+    //SET CS PIN LOW
     //send buffer to SPI
+    spiTransmitData(handl->spi_base_addr, &sConfDat, 1, &value);
+    //SET CS PIN HIGH
 
     handl->control_reg_val = value;
 }
 
-unsigned int adc_get_val(ADC_Handler *handl) {
-    unsigned int value = 0;
-    
+uint16_t adc_get_raw(ADC_Handler *handl) {
+    uint16_t value = 0;
+
+    //SET CS PIN LOW 
     //SPI read into value
+    spiReceiveData(handl->spi_base_addr, &sConfDat, 1, &value);
+    //SET CS PIN HIGH
 
     return value;
 }
--- sensor_software/src/adc_handler.h
+++ sensor_software/src/adc_handler.h
@@ -1,29 +1,32 @@
-/*
- * @brief: ADC Handler - Returns voltage values from specified adc channel
- * 
-*/
 #ifndef ADC_HANDLER_H
 #define ADC_HANDLER_H
 
 #include "stdint.h"
+#include "spi.h"
 
 typedef struct adc_handler_t ADC_Handler;
 
 struct adc_handler_t{
     unsigned short      control_reg_val;   //Current value of the ADC control register
-    const unsigned int  adc_res;           //Maximum resolution of the ADC
+    unsigned int        adc_res;           //Maximum resolution of the ADC
+
+    spiBASE_t          *spi_base_addr;
 };
 
+
 // Initialize ADC defaults and SPI
-void adc_init(ADC_Handler *handl);
+void adc_init(ADC_Handler *handl, spiBASE_t *spi);
 
 //return the raw value from the adc
-unsigned int adc_get_val(ADC_Handler *handl);
+uint16_t adc_get_raw(ADC_Handler *handl);
+
+unsigned int adc_get_temp();
 
 void adc_set_control_reg(ADC_Handler *handl, unsigned short value);
 
 //convert raw value to voltage
 //unsigned int adc_();
 
+//DEMUX PIN SETTING
 
 #endif
