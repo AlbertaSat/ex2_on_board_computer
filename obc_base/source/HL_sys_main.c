fbe84a47ef93fd57f2aa6e0f72287f500b86e2f7
Thomas <ganley@ualberta.ca>
2021-06-28 17:01:36 -0600

    Completed most of sections 3 and 4 of S-band testing plan (data buffer and stress tests

--- obc_base/source/HL_sys_main.c
+++ obc_base/source/HL_sys_main.c
@@ -56,7 +56,9 @@
 
 #include <stdlib.h>
 #include "sTransmitter.h"
-#include "HL_spi.h"
+#include "spi.h"
+
+#include <time.h>
 
 /* USER CODE END */
 
@@ -89,43 +91,131 @@ uint32 	emacPhyAddress	=	1U;
     int j_delay;
     for(j_delay = 0; j_delay < 0x800000; j_delay++);
 
+    srand(time(0));
+    uint8_t stx_return;
     uint8_t pa, mode;
-    uint8_t scrambler, filter, mod, rate;
-    float frequency;
-    int stx_return, TR;
+    stx_return = STX_setControl(0,0);
+    stx_return = STX_setControl(1,1);
+    stx_return = STX_getControl(&pa, &mode);
 
-    stx_return = STX_softResetFPGA();
+//    //3a
+//    uint16_t rand_byte, count;
+//    int tr = 0;
+//
+//    rand_byte = rand() % 65536; // 16 bit number
+//    SPISbandTx(&rand_byte);
+//    stx_return = STX_getTR(&tr);
+//    stx_return = STX_getBuffer(0, &count);
+//
+//    // 3b
+//    int i = 0;
+//    for(i; i < 1280; i++){
+//        rand_byte = rand() % 65536; // 16 bit number
+//        SPISbandTx(&rand_byte);
+//    }
+//
+//    stx_return = STX_getTR(&tr);
+//    stx_return = STX_getBuffer(0, &count);
+//
+//    // 3cP1
+//    i = 0;
+//    uint16_t stop_count, current_count;
+//    stop_count = 500 + (rand() % 4500);
+//    for(i; i < 5000; i++){
+//        rand_byte = rand() % 65536;
+//        SPISbandTx(&rand_byte);
+//        if(i == stop_count){
+//            gioSetBit(hetPORT1, 9, 1); // Disable the S-band CS line
+//            break;
+//        }
+//    }
+//    stx_return = STX_getTR(&tr);
+//    stx_return = STX_getBuffer(0, &count);
+//    current_count = count;
+//    gioSetBit(hetPORT1, 9, 0); // Enable the S-band CS line
+//
+//    // 3cP2
+//    i = 0;
+//    uint16_t overrun;
+//    for(i; i < (20480 - current_count); i++){
+//        // Sending 16 bits at a time, so every second byte
+//        if((i % 2) == 0){
+//            rand_byte = rand() % 65536;
+//            SPISbandTx(&rand_byte);
+//        }
+//    }
+//    stx_return = STX_getTR(&tr);
+//    stx_return = STX_getBuffer(0, &count);
+//    stx_return = STX_getBuffer(2, &overrun);
+//
+//    // 3d
+//    i = 0;
+//    for(i; i < 50; i++){
+//        rand_byte = rand() % 65536;
+//        SPISbandTx(&rand_byte);
+//    }
+//    stx_return = STX_getTR(&tr);
+//    stx_return = STX_getBuffer(0, &count);
+//    stx_return = STX_getBuffer(2, &overrun);
+//
+//
+//    // 4a
+//    int j = 0;
+//    int k = 0;
+//    uint8_t command[2];
+//
+//
+    uint8_t scrambler, filter, mod, rate, power;
+    float freq;
+//
+//    while(k<100){
+//        for(j; j<5; j++){
+//            command[0] = j;
+//            command[1] = rand() % 256;
+//            i2c_sendCommand(0x26, command, 2);
+//        }
+//
+//        stx_return = STX_getControl(&pa, &mode);
+//        stx_return = STX_getEncoder(&scrambler, &filter, &mod, &rate);
+//        stx_return = STX_getPaPower(&power);
+//        stx_return = STX_getFrequency(&freq);
+//
+//        stx_return = STX_softResetFPGA();
+//
+//        stx_return = STX_getControl(&pa, &mode);
+//        stx_return = STX_getEncoder(&scrambler, &filter, &mod, &rate);
+//        stx_return = STX_getPaPower(&power);
+//        stx_return = STX_getFrequency(&freq);
+//        k++;
+//        j = 0;
+//    }
+//
+//    // 4b
+//    uint8_t control_command[2] = {0, 124};
+//    i2c_sendCommand(0x26, control_command, 2);
+//    stx_return = STX_getControl(&pa, &mode);
+//
+//    // 4c
+//    uint8_t encoder_command[3] = {1, 1, 1}; // Effectively 257
+//    i2c_sendCommand(0x26, encoder_command, 3);
+//    stx_return = STX_getEncoder(&scrambler, &filter, &mod, &rate);
 
+    // 4d
     stx_return = STX_getControl(&pa, &mode);
     stx_return = STX_getEncoder(&scrambler, &filter, &mod, &rate);
-    stx_return = STX_getFrequency(&frequency);
+    stx_return = STX_getPaPower(&power);
+    stx_return = STX_getFrequency(&freq);
 
-    pa = 1; // pa on
-    mode = 1; // synch mode
-    stx_return = STX_setControl(pa, mode);
+//    // 4e
+//    i2cSetBaudrate(i2cREG1, 500);
+//    stx_return = STX_getControl(&pa, &mode);
+//    stx_return = STX_getEncoder(&scrambler, &filter, &mod, &rate);
+//    stx_return = STX_getPaPower(&power);
+//    stx_return = STX_getFrequency(&freq);
 
-    stx_return = STX_getControl(&pa, &mode);
-    stx_return = STX_getTR(&TR);
+    // 4f will have to be done within the i2c code I think
 
-    // SPI
-    spiBASE_t * spi_reg = spiREG3; // SPI2
-
-    uint16_t data = 0b0011010100110101;
-    int i;
-    for(i = 0; i<1000000; i++){
-        SPIMasterTx(spi_reg, &data);
-    }
-
-    stx_return = STX_getTR(&TR);
-
-    uint16_t ptr[3];
-    stx_return = STX_getBuffer(0, &ptr[0]);
-    stx_return = STX_getBuffer(1, &ptr[1]);
-    stx_return = STX_getBuffer(2, &ptr[2]);
-
-    pa = 0;
-    mode = 0;
-    stx_return = STX_setControl(pa, mode);
+    stx_return = STX_setControl(0,0);
 
     CLIhandler();
 
